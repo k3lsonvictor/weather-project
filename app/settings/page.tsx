eb22dfea6db9d3cd1b185453c82c2ebc9d48ea86
k3lsonvictor <kelson@wearebren.com>
2024-12-23 23:03:37 -0300

    Remove unused weather components and related hooks from settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,29 +1,8 @@
 "use client";
-import { useWeatherData } from "../hooks/useWeatherData";
-import { TodayWeather } from "../components/TodayWeather/todayWeather";
-import { LastTemperatureCardNew } from "../components/LastTemperatureCard/lastTemperatureCardNew";
-import { WeekWeather } from "../components/WeekWeather/weekWeather";
 
 export default function Home() {
-  const { lastTemperature, temperatureAlongDay, maxAndMinTemperature, weekWeather, timezone } = useWeatherData(52.52, 13.41);
-
   return (
     <div className="flex flex-col md:grid md:grid-cols-[1fr_auto] h-full items-start p-8 gap-4 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col gap-4 w-full h-full">
-        <LastTemperatureCardNew
-          lastTemperature={lastTemperature}
-          timezone={timezone}
-        />
-        <TodayWeather
-          temperatureAlongDay={temperatureAlongDay}
-          maxAndMinTemperature={maxAndMinTemperature}
-        />
-      </div>
-      <div className="w-full h-full">
-        <WeekWeather
-          weekWeather={weekWeather}
-        />
-      </div>
     </div>
   );
 }
