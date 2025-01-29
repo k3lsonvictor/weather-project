43df0bcb68f51f3b102e54eaa9ecfd414ecc3b78
k3lsonvictor <kelson@wearebren.com>
2025-01-29 14:24:55 -0300

    Update API endpoint for leisure recommendations: switch to new weather service URL

--- app/api/callers/weatherAi.tsx
+++ app/api/callers/weatherAi.tsx
@@ -1,5 +1,5 @@
 import { request } from "@/app/api/apiRequester";
 
 export const getLeisureRecommendations = async (values: string) => {
-  return request(`https://minha-app-node-634861991701.us-central1.run.app/generate`, 'POST', { prompt: values });
+  return request(`https://api-studies-1049486429780.us-central1.run.app/generate`, 'POST', { prompt: values });
 };
